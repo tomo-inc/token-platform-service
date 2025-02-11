3c6e03147237b02252e69e246a9eb0041f6c46de
bob.liu <bob.liu@tomo.inc>
2025-02-11 22:27:12 +0800

    delete CorsFilterConfig

--- src/main/java/com/tomo/config/CorsFilterConfig.java
+++ src/main/java/com/tomo/config/CorsFilterConfig.java
@@ -1,18 +1,18 @@
-package com.tomo.config;//package com.tomo.wallet.config;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-public class CorsFilterConfig {
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
-        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new CorsFilter());
-        registration.addUrlPatterns("/*");
-        return registration;
-    }
-}
+//package com.tomo.config;//package com.tomo.wallet.config;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Configuration
+//public class CorsFilterConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+//        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<>();
+//        registration.setFilter(new CorsFilter());
+//        registration.addUrlPatterns("/*");
+//        return registration;
+//    }
+//}
