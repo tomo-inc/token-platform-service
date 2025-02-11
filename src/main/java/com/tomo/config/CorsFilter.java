6484c770560cc7d39a6f54a7ff55a22482393d5c
bob.liu <bob.liu@tomo.inc>
2025-02-11 22:27:12 +0800

    delete CorsFilterConfig

--- src/main/java/com/tomo/config/CorsFilter.java
+++ src/main/java/com/tomo/config/CorsFilter.java
@@ -1,32 +1,32 @@
-package com.tomo.config;
-
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.filter.GenericFilterBean;
-
-import jakarta.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import java.io.IOException;
-
-@WebFilter(urlPatterns = "/*")
-public class CorsFilter extends GenericFilterBean {
-
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "false");
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            filterChain.doFilter(servletRequest, servletResponse);
-        }
-    }
-}
+//package com.tomo.config;
+//
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.web.filter.GenericFilterBean;
+//
+//import jakarta.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import java.io.IOException;
+//
+//@WebFilter(urlPatterns = "/*")
+//public class CorsFilter extends GenericFilterBean {
+//
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "*");
+//        response.setHeader("Access-Control-Allow-Credentials", "false");
+//        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//            response.setStatus(HttpServletResponse.SC_OK);
+//        } else {
+//            filterChain.doFilter(servletRequest, servletResponse);
+//        }
+//    }
+//}
